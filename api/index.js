6eae63de1ff6206646eac8056d5482109402e282
Jeffrey Robinson <jennifersalomi734@gmail.com>
2026-08-29 17:45:18 +0530

    fix: optimize vercel serverless routes, root build script, and mongodb auto-connect

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 const { app } = require('../server/src/app');
+const connectDB = require('../server/src/config/db');
 
-module.exports = (req, res) => {
+let isConnected = false;
+
+module.exports = async (req, res) => {
+  if (!isConnected) {
+    try {
+      await connectDB();
+      isConnected = true;
+    } catch (err) {
+      console.warn('[Vercel Serverless DB Warning]:', err.message);
+    }
+  }
   return app(req, res);
 };
+
